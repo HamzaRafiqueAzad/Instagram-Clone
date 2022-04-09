cde3ded8dcf66ac6e696d59c9ef6a9fb12e04095
Hamza Rafique Azad <hamza.rafiqazad@gmail.com>
2022-04-10 01:30:37 +0500

    Second Commit for Edit Profile View Controller with more configured cells and data.

--- Instagram/Controllers/Other/General/EditProfileViewController.swift
+++ Instagram/Controllers/Other/General/EditProfileViewController.swift
@@ -7,13 +7,30 @@
 
 import UIKit
 
-class EditProfileViewController: UIViewController {
+struct EditProfileFormModel {
+    let label: String
+    let placeholder: String
+    var value: String?
+}
 
+
+/// Edit Profile View Controller
+final class EditProfileViewController: UIViewController {
+    
+    private var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(FormTableViewCell.self, forCellReuseIdentifier: FormTableViewCell.identifier)
+        return tableView
+    }()
+
+    private var models = [[EditProfileFormModel]]()
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         view.backgroundColor = .systemBackground
+        
+        configureModels()
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save",
                                                             style: .done,
@@ -23,13 +40,51 @@ class EditProfileViewController: UIViewController {
                                                             style: .plain,
                                                             target: self,
                                                             action: #selector(didTapCancel))
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.tableHeaderView = createTableHeaderView()
+        view.addSubview(tableView)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+    
+    private func configureModels() {
+        // Name, Username, Website, Bio
+        let section1Labels = ["Name", "Username", "Bio"]
+        var section1 = [EditProfileFormModel]()
+        for label in section1Labels {
+            let model = EditProfileFormModel(label: label,
+                                             placeholder: "Enter \(label)...",
+                                             value: nil)
+            section1.append(model)
+        }
+        models.append(section1)
+        
+        // Email, Phone, Gender
+        let section2Labels = ["Email", "Phone", "Gender"]
+        var section2 = [EditProfileFormModel]()
+        for label in section2Labels {
+            let model = EditProfileFormModel(label: label,
+                                             placeholder: "Enter \(label)...",
+                                             value: nil)
+            section2.append(model)
+        }
+        models.append(section2)
     }
     
     @objc private func didTapSave() {
+        // Save info to database
+        
         
     }
     
     @objc private func didTapCancel() {
+        dismiss(animated: true,
+                completion: nil)
         
     }
     
@@ -38,13 +93,16 @@ class EditProfileViewController: UIViewController {
                                             message: "Change profile picture",
                                             preferredStyle: .actionSheet)
         
-        actionSheet.addAction(UIAlertAction(title: "Take Photo", style: .default, handler: { _ in
+        actionSheet.addAction(UIAlertAction(title: "Take Photo",
+                                            style: .default, handler: { _ in
             
         }))
-        actionSheet.addAction(UIAlertAction(title: "Choose from Library", style: .default, handler: { _ in
+        actionSheet.addAction(UIAlertAction(title: "Choose from Library",
+                                            style: .default, handler: { _ in
             
         }))
-        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil))
+        actionSheet.addAction(UIAlertAction(title: "Cancel",
+                                            style: .default, handler: nil))
         
         actionSheet.popoverPresentationController?.sourceRect = view.bounds
         actionSheet.popoverPresentationController?.sourceView = view
@@ -53,4 +111,70 @@ class EditProfileViewController: UIViewController {
         
     }
 
+}
+
+extension EditProfileViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    private func createTableHeaderView() -> UIView{
+        let header = UIView(frame: CGRect(x: 0,
+                                          y: 0,
+                                          width: view.width,
+                                          height: view.height / 4).integral)
+        
+        let size = header.height / 1.5
+        let profilePhotoButton = UIButton(frame: CGRect(x: (view.width - size) / 2,
+                                                        y: (header.height - size) / 2,
+                                                        width: size,
+                                                        height: size))
+        header.addSubview(profilePhotoButton)
+        profilePhotoButton.layer.masksToBounds = true
+        profilePhotoButton.layer.cornerRadius = size / 2.0
+        profilePhotoButton.addTarget(self, action: #selector(didTapProfilePhoto), for: .touchUpInside)
+        profilePhotoButton.setBackgroundImage(UIImage(systemName: "person.circle"), for: .normal)
+        profilePhotoButton.layer.borderWidth = 1
+        profilePhotoButton.layer.borderColor = UIColor.secondarySystemBackground.cgColor
+        
+        return header
+    }
+    
+    @objc private func didTapProfilePhoto() {
+        
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return models.count
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models[section].count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let model = models[indexPath.section][indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: FormTableViewCell.identifier, for: indexPath) as! FormTableViewCell
+        
+        cell.configure(with: model)
+        cell.delegate = self
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        guard section == 1 else {
+            return nil
+        }
+        return "Private Information"
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
+}
+
+extension EditProfileViewController: FormTableViewCellDelegate {
+    func formTableViewCell(_ cell: FormTableViewCell, didUpdateField updateModel: EditProfileFormModel) {
+        // Update the model
+        print(updateModel.value ?? "nil")
+    }
 }
