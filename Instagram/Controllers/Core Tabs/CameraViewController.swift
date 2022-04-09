ca35f45612a154acf9dd346a5beba3ddeea8f419
Hamza Rafique Azad <hamza.rafiqazad@gmail.com>
2022-04-09 21:57:26 +0500

    Initial Setup of Camera and Explore View Controller.

--- Instagram/Controllers/Core Tabs/CameraViewController.swift
+++ Instagram/Controllers/Core Tabs/CameraViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Hamza Rafique Azad on 9/4/22.
 //
 
+import AVFoundation
 import UIKit
 
 class CameraViewController: UIViewController {
@@ -13,6 +14,11 @@ class CameraViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+    
+    private func didTapTakePicture() {
+        
     }
 
 }
