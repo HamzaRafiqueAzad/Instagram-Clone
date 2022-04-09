499d79881047e4394c24dcfc190070ebbce464d4
Hamza Rafique Azad <hamza.rafiqazad@gmail.com>
2022-04-10 01:33:38 +0500

    Added initial file for Photo CollectionView Cell.

---
+++ Instagram/Views/Cells/PhotoCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoCollectionViewCell.swift
+//  Instagram
+//
+//  Created by Hamza Rafique Azad on 10/4/22.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier = "PhotoCollectionViewCell"
+    
+}
